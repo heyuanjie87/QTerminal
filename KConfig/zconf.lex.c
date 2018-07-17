571b98711a8652fb6aba54b327edf50ed3b97036
heyuanjie <xx>
2018-07-17 11:05:34 +0800

    代码清理

--- KConfig/zconf.lex.c
+++ KConfig/zconf.lex.c
@@ -2885,17 +2885,10 @@ static void zconf_endhelp(YYSTYPE *lval, yyscan_t yyscanner)
  */
 FILE *zconf_fopen(const char *name)
 {
-    char *env, fullname[PATH_MAX+1];
     FILE *f;
 
     f = fopen(name, "r");
-    if (!f && name != NULL && name[0] != '/') {
-        env = getenv(SRCTREE);
-        if (env) {
-            sprintf(fullname, "%s/%s", env, name);
-            f = fopen(fullname, "r");
-        }
-    }
+
     return f;
 }
 
